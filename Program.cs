bf35a4ddf632c0784c31ea7d2099239b5b882d71
NovaGamma <gammanova41@gmail.com>
2023-11-23 11:30:11 +0100

    Load csv and read all ticks

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader("tradesoft-ticks-sample.csv"))
+            {
+                List<Tick> tickList = new List<Tick>();
+                string line = reader.ReadLine();
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var values = line.Split(',');
+                    DateTime date = Convert.ToDateTime(values[0]);
+                    string type = values[1];
+                    Int32 quantity = Convert.ToInt32(values[2]);
+                    float price = Convert.ToSingle(values[3], System.Globalization.CultureInfo.InvariantCulture);
+                    tickList.Add(new Tick(date,type,quantity,price));
+                }
+                Console.WriteLine(tickList.Count);
+            }
+        }
+    }
+}
+
+class Tick
+{
+    public DateTime time;
+    public string type;
+    public int quantity;
+    public float price;
+
+    public Tick(DateTime time, string type, int quantity, float price)
+    {
+        this.time = time;
+        this.type = type;
+        this.quantity = quantity;
+        this.price = price;
+    }
+}
+
