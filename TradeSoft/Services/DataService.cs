f64717320925bb2ec6275fec116764abdbbd4c18
ZeloteX <thomascw974@gmail.com>
2023-12-15 17:17:22 +0100

    Create DataService.cs

---
+++ TradeSoft/Services/DataService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+using TradeSoft.Models;
+
+
+namespace TradeSoft.Services
+{
+    internal class DataService
+    {
+        public void FetchData()
+        {
+            using (var reader = new StreamReader("tradesoft-ticks-sample.csv"))
+            {
+                List<Tick> tickList = new List<Tick>();
+                string line = reader.ReadLine();
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var values = line.Split(',');
+                    DateTime date = Convert.ToDateTime(values[0]);
+                    string type = values[1];
+                    Int32 quantity = Convert.ToInt32(values[2]);
+                    float price = Convert.ToSingle(values[3], System.Globalization.CultureInfo.InvariantCulture);
+                    tickList.Add(new Tick(date, type, quantity, price));
+                }
+                Console.WriteLine(tickList.Count);
+            }
+        }
+    }
+}
