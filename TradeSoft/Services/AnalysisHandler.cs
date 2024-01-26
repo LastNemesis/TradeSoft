52aef9657dca20df562865d4e558af24e95fdffd
NovaGamma <gammanova41@gmail.com>
2024-01-26 13:25:29 +0100

    Create AnalysisHandler.cs

---
+++ TradeSoft/Services/AnalysisHandler.cs
@@ -0,0 +1,27 @@
+﻿using TradeSoft.Models;
+
+namespace TradeSoft.Services
+{
+    public class AnalysisHandler
+    {
+        private Dictionary<int, Analysis> _strategiesAnalysis = new Dictionary<int, Analysis>();
+        private Logger _logger;
+
+        public AnalysisHandler(List<int> stratgiesId, Logger logger) {
+            _logger  = logger;
+            foreach (var strategyId in stratgiesId)
+            {
+                _strategiesAnalysis[strategyId] = new Analysis();
+            }
+        }
+
+        public void analyseExecutionBits(List<Order> executedOrders)
+        {
+            foreach (var order in executedOrders)
+            {
+                const String analysis = _strategiesAnalysis[order.StratId].runMethods(order.ExecutionBits[^1])
+                _logger.LogAnalysis(order, analysis);
+            }
+        }
+    }
+}
